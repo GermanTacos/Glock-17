e5e32909949fb0b678dc4467f5f4a6412e85df5c
IKillerwolfI <killerwolf.no1@gmail.com>
2018-11-04 03:52:09 +0100

    Fixes
    welp

--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -4,13 +4,13 @@ if ( self.glawk ) then
 	self:SetupAttachmentPoint("glawk", {
 		name = "a_ns", 
         base_a_obj = "a_ns", 
-        position = Vector3( 0, -0.57, -0.6 ), 
+        position = Vector3( 0, -0.57, -0.65 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
 	self:SetupAttachmentPoint("glawk", {
 		name = "a_co", 
         base_a_obj = "a_co", 
-        position = Vector3( 0, -0.57, -0.6 ), 
+        position = Vector3( 0, -0.57, -0.65 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
 end
