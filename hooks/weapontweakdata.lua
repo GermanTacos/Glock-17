8242fdc077daffe56594b7831252817e7d1db227
IKillerwolfI <killerwolf.no1@gmail.com>
2018-11-04 03:08:27 +0100

    Fixed Barrel Exts
    -CZ75 >:(

--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -1,84 +1,17 @@
-Hooks:PostHook( WeaponTweakData, "init", "CZ-75BmodInit", function(self)
+Hooks:PostHook( WeaponTweakData, "init", "Glock17modInit", function(self)
 
-if ( self.cz75b ) then
-    self.cz75b.attachment_points = {
-        {
-            name = "cz75bfl_rl",
-            base_a_obj = "a_body",
-            position = Vector3( 0, -1, -1 ), 
-            rotation = Rotation( 0, 0, 0 )
-        },
-        {
-            name = "cz75bfl",
-            base_a_obj = "a_fl",
-            position = Vector3( 0, 1.8, -1 ), 
-            rotation = Rotation( 0, 0, 0 )
-        },
-        {
-            name = "a_cz75bns", 
-            base_a_obj = "a_ns",
-            position = Vector3( 0, 1, -0.5 ),
-            rotation = Rotation( 0, 0, 0 ) 
-        },
-        {
-            name = "a_cz75bns_ext", 
-            base_a_obj = "a_ns",
-            position = Vector3( 0, 1.7, -0.5 ),
-            rotation = Rotation( 0, 0, 0 ) 
-        },
-        {
-            name = "a_cz75bns_c", 
-            base_a_obj = "a_ns",
-            position = Vector3( 0, -0.5, -0.5 ),
-            rotation = Rotation( 0, 0, 0 ) 
-        },
-        {
-            name = "a_cz75bns_ext_c", 
-            base_a_obj = "a_ns",
-            position = Vector3( 0, 0.5, -0.5 ),
-            rotation = Rotation( 0, 0, 0 ) 
-        }
-    }
+if ( self.glawk ) then
+	self:SetupAttachmentPoint("glawk", {
+		name = "a_ns", 
+        base_a_obj = "a_ns", 
+        position = Vector3( 0, -0.57, -0.6 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("glawk", {
+		name = "a_co", 
+        base_a_obj = "a_co", 
+        position = Vector3( 0, -0.57, -0.6 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
 end
-if ( self.x_cz75b ) then
-    self.x_cz75b.attachment_points = {
-        {
-            name = "cz75bfl_rl",
-            base_a_obj = "a_body",
-            position = Vector3( 0, -1, -1 ), 
-            rotation = Rotation( 0, 0, 0 )
-        },
-        {
-            name = "cz75bfl",
-            base_a_obj = "a_fl",
-            position = Vector3( 0, 1.8, -1 ), 
-            rotation = Rotation( 0, 0, 0 )
-        },
-        {
-            name = "a_cz75bns", 
-            base_a_obj = "a_ns",
-            position = Vector3( 0, 1.1, -0.5 ),
-            rotation = Rotation( 0, 0, 0 ) 
-        },
-        {
-            name = "a_cz75bns_ext", 
-            base_a_obj = "a_ns",
-            position = Vector3( 0, 1.7, -0.5 ),
-            rotation = Rotation( 0, 0, 0 ) 
-        },
-        {
-            name = "a_cz75bns_c", 
-            base_a_obj = "a_ns",
-            position = Vector3( 0, -0.5, -0.5 ),
-            rotation = Rotation( 0, 0, 0 ) 
-        },
-        {
-            name = "a_cz75bns_ext_c", 
-            base_a_obj = "a_ns",
-            position = Vector3( 0, 0.5, -0.5 ),
-            rotation = Rotation( 0, 0, 0 ) 
-        }
-    }
-end
-
 end )
