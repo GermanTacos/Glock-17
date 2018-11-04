4c6940dd942e2eeec67722406a4f689a10ac6fee
IKillerwolfI <killerwolf.no1@gmail.com>
2018-11-04 10:54:46 +0100

    Barrel Ext CAP
    -Added CAP for Barrel Exts on threaded barrels

--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -8,6 +8,12 @@ if ( self.glawk ) then
         rotation = Rotation( 0, 0, 0 ) 
 	})
 	self:SetupAttachmentPoint("glawk", {
+		name = "a_ns_ext", 
+        base_a_obj = "a_ns", 
+        position = Vector3( 0, -0.3, -0.65 ), 
+        rotation = Rotation( 0, 0, 0 ) 
+	})
+	self:SetupAttachmentPoint("glawk", {
 		name = "a_co", 
         base_a_obj = "a_co", 
         position = Vector3( 0, -0.57, -0.65 ), 
